2b6040f0f2591c12fbcb8f091f53b1a8e9ae6cb5
kinacororinn <kinacororinn@gmail.com>
2021-04-11 10:21:14 +0000

    bookers 完成

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,9 @@ Rails.application.routes.draw do
  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
  resources :books
 get 'top' => 'books#top'
-get 'index' => 'books#index'
+get 'books' => 'books#index'
 post 'homes' => 'homes#create'
 post 'books' => 'books#edit'
+root to: 'books#top'
 end
 
